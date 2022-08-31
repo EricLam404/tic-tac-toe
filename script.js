9190f1f745373862843075cec2d24c6d352402e1
EricLam404 <lameric9888@gmail.com>
2022-08-31 18:10:38 -0400

    Added check winner

--- script.js
+++ script.js
@@ -19,14 +19,37 @@ const Board = () => {
 
     function placeMove(e){
         e.target.textContent = turn;
+        let boxNum = e.target.classList[1] - 1;
+        board[boxNum] = turn;
         changeTurn();
+        console.log(checkWinner());
         e.target.removeEventListener("click", placeMove);
     }
+
     function changeTurn(){
         if(turn === "X"){
             turn = "O";
         }else{
             turn = "X";
+        }
+    }
+
+    function checkWinner(){
+        for(let i = 0; i < 3; i++){
+            if(board[i] == board[i+3] && board[i] == board[i+6]){
+                return board[i];
+            }
+        }
+        for(let i = 0; i <= 6; i += 3){
+            if(board[i] == board[i+1] && board[i] == board[i+2]){
+                return board[i];
+            }
+        }
+        if(board[0] == board[4] && board[0] == board[8]){
+            return board[0];
+        }
+        if(board[2] == board[4] && board[2] == board[6]){
+            return board[2];
         }
     }
 
