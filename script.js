836041dae4b2f81232aefb2a3c886b7bbbc4907c
EricLam404 <lameric9888@gmail.com>
2022-08-31 19:37:47 -0400

    Added a reset button

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const Board = () => {
     let board = ["", "", "", "", "", "", "", "", ""];
     let boxes = document.querySelectorAll(".box");
     let turn = "X";
+    let count = 0;
 
     const displayBoard = () => {
         boxes.forEach(box => {
@@ -9,6 +10,16 @@ const Board = () => {
 
             box.textContent = board[boxNum];
         });
+    }
+
+    const resetBoard = () => {
+        board = ["", "", "", "", "", "", "", "", ""];
+        turn = "X";
+        count = 0;
+        displayBoard();
+        addOnClick();
+        let display = document.querySelector(".turn");
+        display.textContent = turn + "'s turn";
     }
 
     const addOnClick = () => {
@@ -22,21 +33,27 @@ const Board = () => {
         let boxNum = e.target.classList[1] - 1;
         board[boxNum] = turn;
         changeTurn();
-        e.target.removeEventListener("click", placeMove);
+        e.target.removeEventListener("click", placeMove)
         if(checkWinner()){
             displayWinner(checkWinner());
         }
     }
 
     function changeTurn(){
+        let display = document.querySelector(".turn");
         if(turn === "X"){
             turn = "O";
         }else{
             turn = "X";
         }
+        display.textContent = turn + "'s turn";
+        count++;
     }
 
     function checkWinner(){
+        if(count == 9){
+            return "tie";
+        }
         for(let i = 0; i < 3; i++){
             if(board[i] == board[i+3] && board[i] == board[i+6]){
                 return board[i];
@@ -53,13 +70,22 @@ const Board = () => {
         if(board[2] == board[4] && board[2] == board[6]){
             return board[2];
         }
+        return undefined;
     }
 
     function displayWinner(winner){
         removeOnClick();
         let container = document.querySelector(".winner");
+        if(winner === "tie"){
+            container.textContent = "It's a tie";
+        }
+        else{
+            container.textContent = "The winner is " + winner;
+        }
 
-        container.textContent = "The winner is " + winner;
+        boxes.forEach(box => {
+            box.classList.add('won');
+        });
     }
 
     function removeOnClick(){
@@ -68,7 +94,7 @@ const Board = () => {
         });
     }
 
-    return {board, displayBoard, addOnClick};
+    return {board, displayBoard, addOnClick, resetBoard};
 }; 
 
 const disableselect = (e) => {  
@@ -81,4 +107,9 @@ const board = Board();
 
 board.addOnClick();
 board.displayBoard();
+
+let reset = document.querySelector(".reset");
+
+reset.addEventListener('click', board.resetBoard);
+
 
